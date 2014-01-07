ddccdbd3be2386229926cd5549809e8ac9a0c7b4
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-01-06 19:42:27 -0500

    stripe token generating

--- public/js/register.js
+++ public/js/register.js
@@ -1,5 +1,5 @@
 $(function(){
-  console.log(window.location.search.indexOf("wind"))
+  // load the plan choice
   if(window.location.search.indexOf("paddle") !== -1){
     $(':radio[value="paddle"]').attr('checked', 'checked');
   }
@@ -10,17 +10,37 @@ $(function(){
   else if(window.location.search.indexOf("steam" !== -1)){
     $(':radio[value="steam"]').attr('checked', 'checked');
   }
+
+  // submit form
   $('#registerDoneButton').click(function(){
-    var email = $('#'),
-        password = $('#'),
-        password2 = $('#'),
-        cardType = $('#'),
-        cardNumber = $('#'),
-        cardExpirationDate = $('#'),
-        cardSecurityCode = $('#'),
-        email = $('#'),
-        email = $('#'),
-        email = $('#')
+    var email = $('#emailInput').val(),
+        password = $('#password1Input').val(),
+        password2 = $('#password2Input').val(),
+        cardType = $('#cardTypeInput').val(),
+        cardNumber = $('#cardNumberInput').val(),
+        cardExpirationDate = $('#cardExpirationDateInput').val(),
+        cardSecurityCode = $('#cardSecurityCodeInput').val(),
+        plan = $('#planInput').val()
+
   })
+
+var handler = StripeCheckout.configure({
+  key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
+  image: 'https://drupal.org/files/styles/grid-3/public/project-images/Very-Basic-Checked-checkbox-icon.png?itok=0nrFxSo_',
+  token: function(token, args) {
+    alert(token)
+    // Use the token to create the charge with a server-side script.
+  }
+});
+
+document.getElementById('registerDoneButton').addEventListener('click', function(e) {
+  // Open Checkout with further options
+  handler.open({
+    name: 'Demo Site',
+    description: '2 widgets ($20.00)',
+    amount: 2000
+  });
+  e.preventDefault();
+  });
 })
 
